f550dddbdb2ac5a6138f438ed2079bf4aa36adfe
wkdgdusdn1016 <wkdgusdn1016@naver.com>
2025-07-17 12:59:26 +0900

    크롤링 useragent 변경

--- dags/eth_test_crawling.py
+++ dags/eth_test_crawling.py
@@ -23,8 +23,13 @@ dag = DAG(
 
 def test_crawl_eth_whales(**kwargs):
     BASE_URL = "https://etherscan.io/accounts"
-    HEADERS = {"User-Agent": "Mozilla/5.0"}
-
+    HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0.0.0 Safari/537.36"
+    )
+}
     def parse_wallets_from_page(html):
         soup = BeautifulSoup(html, "html.parser")
         table = soup.find("table")
