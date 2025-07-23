071c3e488d2e9b6016c4dcecb5b2d464bdce5ac5
당ìJeongInSon <dzn05@naver.com>
2025-07-23 18:19:56 +0900

    eth_whale_dag update

--- dags/eth_whale_dag.py
+++ dags/eth_whale_dag.py
@@ -35,12 +35,22 @@ dag = DAG(
 def crawl_and_save_csv(**kwargs):
     BASE_URL = "https://etherscan.io/accounts"
     HEADERS = {
-        "User-Agent": (
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-            "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/115.0.0.0 Safari/537.36"
-        )
-    }
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/115.0.0.0 Safari/537.36"
+    ),
+    "Accept": (
+        "text/html,application/xhtml+xml,application/xml;"
+        "q=0.9,image/webp,*/*;q=0.8"
+    ),
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://etherscan.io/",
+    "Connection": "keep-alive",
+}
+
+
 
     def parse_wallets_from_page(html):
         soup = BeautifulSoup(html, "html.parser")
