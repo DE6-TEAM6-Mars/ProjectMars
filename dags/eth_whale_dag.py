4faca53d9c8ccc651fb29c64f3d7ecce27a4cf92
당ìJeongInSon <dzn05@naver.com>
2025-07-24 13:17:24 +0900

    eth_whale_dag update

--- dags/eth_whale_dag.py
+++ dags/eth_whale_dag.py
@@ -36,19 +36,21 @@ dag = DAG(
 def crawl_and_save_csv(**kwargs):
     BASE_URL = "https://etherscan.io/accounts"
     HEADERS = {
-    "User-Agent": (
+        "User-Agent": (
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
-            "Chrome/115.0.0.0 Safari/537.36"
+            "Chrome/123.0.0.0 Safari/537.36"
         ),
-        "Accept": (
-            "text/html,application/xhtml+xml,application/xml;"
-            "q=0.9,image/webp,*/*;q=0.8"
-        ),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Language": "en-US,en;q=0.9",
         "Referer": "https://etherscan.io/",
         "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-User": "?1",
     }
 
     scraper = cloudscraper.create_scraper()
